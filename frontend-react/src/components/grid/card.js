f0742e6e201286a949e4be85d77631c2eacce37f
Alex Pho <pho.a@husky.neu.edu>
2021-03-26 00:13:16 -0400

    cleanup.

--- frontend-react/src/components/grid/card.js
+++ frontend-react/src/components/grid/card.js
@@ -3,72 +3,22 @@ import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Col} from "react-bootstrap";
 
-const ResultCard = (
-    {
-        result
-    }) => {
-
-    // const [editing, setEditing] = useState(false)
-    // const [newTitle, setNewTitle] = useState(title)
-
-    // const [hover, setHover] = useState(false)
-    //
-    // const onHover = () => {
-    //     setHover(true)
-    // }
-    //
-    // const onLeave = () => {
-    //     setHover(false)
-    // }
-
-    // const saveTitle = () => {
-    //     setEditing(false)
-    //     const newCourse = {
-    //         ...course,
-    //         title: newTitle
-    //     }
-    //     updateCourse(newCourse)
-    //     setNewTitle("")
-    // }
+const ResultCard = ({ name, to }) => {
 
     return (
         <Col xs={12} sm={6} md={4} lg={3} xl={2}>
             <div className="card mt-4">
-                 {/*// onMouseEnter={onHover} onMouseLeave={onLeave}>*/}
                 <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
                      className="card-img-top"
                      alt="..."/>
                 <div className="card-body">
-                    {/*{editing && <FontAwesomeIcon icon={"trash"} size={"lg"} pull={"right"}*/}
-                    {/*                             onClick={() => {*/}
-                    {/*                                 deleteCourse(course);*/}
-                    {/*                                 setEditing(false);*/}
-                    {/*                             }}/>*/}
-                    {/*}*/}
-                    {/*{editing && <FontAwesomeIcon icon={faCheck} size={"lg"} pull={"right"} className="mr-1"*/}
-                    {/*                             onClick={() => saveTitle()}/>}*/}
-
-                    <h5 className="card-title">{result}</h5>
+                    <h5 className="card-title">{name}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of
                         the
                         card's
                         content.</p>
                     <img src={``}/>
-
-                    {/*{*/}
-                    {/*    !editing &&*/}
-                    {/*    <Link to={`/courses/grid/edit/${result._id}`} className="btn btn-primary">{title}</Link>*/}
-                    {/*}*/}
-                    {/*{*/}
-                    {/*    editing &&*/}
-                    {/*    <input*/}
-                    {/*        onChange={(event) => setNewTitle(event.target.value)}*/}
-                    {/*        value={newTitle}*/}
-                    {/*        className="form-control"/>*/}
-                    {/*}*/}
-                    {/*{hover && !editing ?*/}
-                    {/*    <FontAwesomeIcon icon={"edit"} size={"lg"} pull={"right"} color="purple" className={"mt-3"}*/}
-                    {/*                     onClick={() => setEditing(true)}/> : null}*/}
+                        <Link to={to} className="btn btn-primary">{name}</Link>
                 </div>
             </div>
         </Col>
--- frontend-react/src/components/grid/grid.js
+++ frontend-react/src/components/grid/grid.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import ResultCard from "./card";
 import {Row} from "react-bootstrap";
-// import {Container, Row} from "react-bootstrap";
 
 
 const ResultGrid = (
@@ -11,28 +8,17 @@ const ResultGrid = (
         results
     }) => {
     return (
-        <div className="my-2">
-            <div className="text-right">
-                <div className={"row text-left"}>
-                    <div className={"col-6"}>
-                        <h4>Recent Documents</h4>
-                    </div>
-                    <div className={"col-3 d-none d-md-inline"}>
-                        <h4>Owned by me</h4>
-                    </div>
-                </div>
-            </div>
-            <Row>
-                {
-                    results.map((result, index) =>
-                        <ResultCard
-                            result={result}
-                            key={index}
-                        />
-                    )
-                }
-            </Row>
-        </div>
+        <Row>
+            {
+                results.map((result) =>
+                    <ResultCard
+                        name={result.attributes.name}
+                        key={result.id}
+                        to={`/details/${result.id}`}
+                    />
+                )
+            }
+        </Row>
     )
 }
 
