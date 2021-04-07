1f103fd6d8270ce1a9ffd4f20e80242dc69fd415
Alex Pho <pho.a@husky.neu.edu>
2021-04-06 22:08:16 -0400

    updates to dependencies

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,6 +2,11 @@ import Search from "./components/search";
 import Details from "./components/details";
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from "./components/Home";
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+import {fas} from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas);
 
 function App() {
   return (
