aa74d86c149293dae4cf68e2dc52f0e5bda5dccd
Alex Pho <pho.a@husky.neu.edu>
2021-04-07 00:03:37 -0400

    update homepage for conditional anon and logged in rendering

--- frontend-react/src/components/Home.js
+++ frontend-react/src/components/Home.js
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from "react-router-dom";
 import LoginPage from "./login/login-page";
+import AnonymousContent from "./anonymous-content";
+import Search from "./search";
+import {Button, Col, Container, Row} from "react-bootstrap";
 
 const Home = () => {
+
+    const [isLoggedIn, setLoginState] = useState(false)
+
+    // useEffect(() => {
+    // }, [isLoggedIn])
+
     return(
-        <div>
-            <h1>Home</h1>
-            <Link to="/search">
-                Search
-            </Link>
-            <br/>
-            <Link to="/login">
-                Login
-            </Link>
-            <br/>
-            <Link to="/register">
-                Sign Up
-            <Link to="/feed">
-                Feed
-            </Link>
-        </div>
+        <Container>
+            <Row className={"mt-4"}>
+                <Col>
+                    <h1>Home</h1>
+                </Col>
+                <Link to={`/login`}>
+                    <Button size={"lg"} variant={"outline-primary"} className={"mr-2"}>
+                        Login
+                    </Button>
+                </Link>
+                <Link to={`/register`}>
+                    <Button size={"lg"} variant={"outline-dark"}>
+                        Sign Up
+                    </Button>
+                </Link>
+                <Link to={`/register`}>
+                    Feed
+                </Link>
+            </Row>
+            <Row className={"justify-content-center"}>
+                <Col lg={6} >
+                    <Search/>
+                </Col>
+            </Row>
+            <Row className={"justify-content-center mt-4"}>
+                <Col>
+                    {
+                        !isLoggedIn &&
+                        <AnonymousContent/>
+                    }
+                    {
+                        isLoggedIn &&
+                        <h1>Should show protected content here</h1>
+                    }
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
