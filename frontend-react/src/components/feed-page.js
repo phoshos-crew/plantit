f3433a10022b92dc05a5e4e6b12aa99651b520b9
Juan Alfaro <alfaro.j@husky.neu.edu>
2021-04-07 00:03:16 -0400

    Adds updates to feed

--- frontend-react/src/components/feed-page.js
+++ frontend-react/src/components/feed-page.js
@@ -2,11 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 import Plant from "./plant";
 import {Row} from 'react-bootstrap'
+import Update from "./update";
 
-const FeedPage = ({plants}) => {
-    return(
+const FeedPage = ({plants, updates}) => {
+    return (
         <div>
             <h1>Feed</h1>
+            {
+                updates.map((update, index) =>
+                    <Row key={index}>
+                        <Update action={update.action} userName={update.userName}/>
+                    </Row>
+                )
+            }
             <Row>
                 {
                     plants.map(crop =>
@@ -19,7 +27,8 @@ const FeedPage = ({plants}) => {
 }
 
 const stpm = (state) => ({
-    plants: state.userReducer.plants
+    plants: state.userReducer.plants,
+    updates: state.userReducer.updates
 })
 
 export default connect(stpm)(FeedPage)
--- frontend-react/src/reducers/user-reducer.js
+++ frontend-react/src/reducers/user-reducer.js
@@ -4,6 +4,16 @@ const initialState = {
         {id: "54c7650e6236310003000000"},
         {id: "54bda00e3961370003150400"},
         {id: "544c88bd3432630002000000"}
+    ],
+    updates: [
+        {
+            userName: "Sravni",
+            action: "NEW_PLANT"
+        },
+        {
+            userName: "Sravni",
+            action: "NEW_PICTURE"
+        }
     ]
 }
 
