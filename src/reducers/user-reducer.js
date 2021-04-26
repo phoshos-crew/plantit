8974ad1f3f71502d697a0808865a4a2de58ff22d
sravanikumar <kumar.sra@northeastern.edu>
2021-04-25 20:02:43 -0400

    completed login validation

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -12,6 +12,10 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case LOGIN:
+            return {
+                ...state,
+                currentUser: action.currentUser
+            }
         case PROFILE:
             return {
                 ...state,
