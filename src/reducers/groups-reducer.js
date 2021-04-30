2518d449478714b170df07855ba7065f480fea1a
chandhini101 <81103060+chandhini101@users.noreply.github.com>
2021-04-30 19:48:31 -0400

    group minor changes (#46)
    Co-authored-by: chandhini101 <sundaram.c@northeastern.edu>

--- src/reducers/groups-reducer.js
+++ src/reducers/groups-reducer.js
@@ -1,15 +1,10 @@
-import {FIND_GROUP_BY_ID, PROFILE} from "../actions/groups-actions";
+import {FIND_GROUP_BY_ID} from "../actions/groups-actions";
 
 
 const initialState = {}
 
 const groupReducer = (state = initialState, action) => {
     switch (action.type) {
-        case PROFILE:
-            return {
-                ...state,
-                currentGroup: action.currentGroup
-            }
         case FIND_GROUP_BY_ID:
             return {
                 ...state,
--- src/services/groups-service.js
+++ src/services/groups-service.js
@@ -1,21 +1,5 @@
 export const PLANTIT_API_URL = 'http://localhost:4000/api';
 
-const profile = () => {
-    return fetch(`${PLANTIT_API_URL}/profile`,{
-        method: "POST",
-        credentials: "include",
-        headers: {
-            'content-type': 'application/json'
-        },
-    })
-        .then(response => {
-            if (response.status === 403) {
-                return null
-            } else {
-                return response.json()
-            }
-        })
-}
 
 const findGroupById = (groupId) => {
     return fetch(`${PLANTIT_API_URL}/groups/${groupId}`)
@@ -24,7 +8,6 @@ const findGroupById = (groupId) => {
 
 
 const api = {
-    profile,
     findGroupById,
 }
 
