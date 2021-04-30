a40317b6432d25ea92e43a075d4798eadb991ca3
sravanikumar <kumar.sra@northeastern.edu>
2021-04-30 19:59:59 -0400

    added responsiveness to top bar

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -3,7 +3,7 @@ import userActions from "../../actions/user-actions";
 import {connect} from "react-redux";
 import {Link, useHistory} from "react-router-dom"
 import Search from "../search";
-import {Button} from "react-bootstrap";
+import {Button, Nav, Navbar, NavDropdown, NavItem} from "react-bootstrap";
 import userService from "../../services/user-service"
 
 const TopBar = ({logout, user}) => {
@@ -20,60 +20,87 @@ const TopBar = ({logout, user}) => {
 
     return(
         <div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Navbar.Brand href={curUser ? "/feed" : "/"}>PlantIt</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        {
+                            curUser &&
+                            <Navbar.Text className={"mx-3 nav-item"}>{`Hello ${curUser.firstName}!`}</Navbar.Text>
+                        }
+                    </Nav>
+                    <form className="form-inline mx-auto">
+                        <Search/>
+                    </form>
+                    <Nav>
+                        {
+                            !curUser &&
+                            <>
+                                <Nav.Link href={`/login`}>
+                                    <Button variant={"outline-primary"} className={"mr-2"}>
+                                        Login
+                                    </Button>
+                                </Nav.Link>
+                                <Nav.Link href={`/register`}>
+                                    <Button variant={"outline-dark"}>
+                                        Sign Up
+                                    </Button>
+                                </Nav.Link>
+                            </>
+                        }
+                        {
+                            curUser && curUser.role === 'SITE_ADMIN' &&
+                            <Link href={`/admin`}>
+                                <Button variant={"danger"} className={"mr-4"}>
+                                    Admin
+                                </Button>
+                            </Link>
+                        }
+                        {
+                            curUser &&
+                            <>
+                                <Nav.Link href={`/profile`}>
+                                    <Button variant={"outline-success"} className={"mr-2"}>
+                                        Profile
+                                    </Button>
+                                </Nav.Link>
+                                <Nav.Link href={`/`}>
+                                    <Button variant={"outline-primary"}
+                                            onClick={() => {
+                                                logout()
+                                                setCurUser(null)
+                                                history.push("/")
+                                            }}>
+                                        Logout
+                                    </Button>
+                                </Nav.Link>
+                            </>
+                        }
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             {/*{console.log("inside", curUser)}*/}
-            <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href={curUser ? "/feed" : "/"}>PlantIt</a>
-                {
-                    curUser &&
-                    <span className={"mx-3"}>{`Hello ${curUser.firstName}!`}</span>
-                }
-                <form className="form-inline mx-auto">
-                    <Search/>
-                </form>
-                {
-                    !curUser &&
-                    <>
-                        <Link to={`/login`}>
-                            <Button variant={"outline-primary"} className={"mr-2"}>
-                                Login
-                            </Button>
-                        </Link>
-                        <Link to={`/register`}>
-                            <Button variant={"outline-dark"}>
-                                Sign Up
-                            </Button>
-                        </Link>
-                    </>
-                }
-                {
-                    curUser && curUser.role === 'SITE_ADMIN' &&
-                    <Link to={`/admin`}>
-                        <Button variant={"danger"} className={"mr-4"}>
-                            Admin
-                        </Button>
-                    </Link>
-                }
-                {
-                    curUser &&
-                        <div>
-                            <Link to={`/profile`}>
-                                <Button variant={"outline-success"} className={"mr-2"}>
-                                    Profile
-                                </Button>
-                            </Link>
-                            <Link to={`/`}>
-                                <Button variant={"outline-primary"}
-                                        onClick={() => {
-                                            logout()
-                                            setCurUser(null)
-                                            history.push("/")
-                                        }}>
-                                    Logout
-                                </Button>
-                            </Link>
-                        </div>
-                }
-            </nav>
+            {/*<nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">*/}
+            {/*    <a className="navbar-brand" href={curUser ? "/feed" : "/"}>PlantIt</a>*/}
+            {/*    <button className="navbar-toggler" type="button" data-toggle="collapse"*/}
+            {/*            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"*/}
+            {/*            aria-expanded="false" aria-label="Toggle navigation">*/}
+            {/*        <span className="navbar-toggler-icon"></span>*/}
+            {/*    </button>*/}
+            {/*    <div className="collapse navbar-collapse" id="navbarSupportedContent">*/}
+            {/*        {*/}
+            {/*            curUser &&*/}
+            {/*            <span className={"mx-3 nav-item"}>{`Hello ${curUser.firstName}!`}</span>*/}
+            {/*        }*/}
+            {/*        <form className="form-inline mx-auto">*/}
+            {/*            <Search/>*/}
+            {/*        </form>*/}
+
+
+            {/*    </div>*/}
+            {/*</nav>*/}
         </div>
     )
 }
