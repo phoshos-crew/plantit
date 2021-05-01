f5e2c4a0a5bb8a5e6adcf14c6cde6a96ad6d46c1
sravanikumar <58430341+sravanikumar@users.noreply.github.com>
2021-05-01 00:00:37 -0400

    added group redux to wireframe (#50)
    Co-authored-by: sravanikumar <kumar.sra@northeastern.edu>

--- src/components/group/group-page.js
+++ src/components/group/group-page.js
@@ -3,67 +3,68 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link, useParams} from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import {Button} from "react-bootstrap";
-// import
+import groupActions from "../../actions/groups-actions";
+import {connect} from "react-redux";
 
 
 const GroupPage = (
     {
-        // group,
-        // findGroupById,
+        group,
+        findGroupById,
     }) => {
 
-    // const {groupId} = useParams()
-    // useEffect(() => {}, [groupId])
-
+    const {groupId} = useParams()
+    useEffect(() => {
+        findGroupById(groupId)
+    }, [groupId])
+    console.log("group", group)
     return (
         <div className={"container"}>
+
             <div className={"row"}>
                 <div className={"col"}></div>
                 <div className={"col-10"}>
-                    {/*<h1>Welcome to {group.groupName}</h1>*/}
-                    <h3 className={"text-center"}>Welcome to Splendid Succulents!</h3>
+                    <h3 className={"text-center"}>Welcome to {group.groupName}!!</h3>
                     <div className="row">
-                        <img src="https://media.istockphoto.com/photos/miniature-succulent-plants-picture-id510059940" height={"100px"} width={"100px"} />
+                        <img src={group.groupPicture} height={"200px"} width={"200px"}/>
                         <br/>
                     </div>
                     <div className="row">
                         <p className={"lead"}><strong>
-                            Succulents are so easy to care for!
+                            {group.groupDesc}
                         </strong></p>
-                        {/*<p>{group.groupDesc}</p>*/}
+
                     </div>
-                    <div className="row">
-                        <h5>Members:</h5>
-                        {/*<div className="col-2">*/}
-                        {/*    <FontAwesomeIcon icon={"circle"}/>*/}
-                        {/*</div>*/}
-                        {/*{*/}
-                        {/*    group.groupMembers &&*/}
-                        {/*    <div className="row">*/}
-                        {/*        {*/}
-                        {/*            group.groupMembers.map(user =>*/}
-                        {/*                <>*/}
-                        {/*                    <Card style={{width: '18rem'}}>*/}
-                        {/*                        <Card.Body>*/}
-                        {/*                            <Card.Title>{user.username}</Card.Title>*/}
-                        {/*                            <Link to={`/profile/${user._id}`}>*/}
-                        {/*                                <Button variant="primary">Go</Button>*/}
-                        {/*                            </Link>*/}
-                        {/*                        </Card.Body>*/}
-                        {/*                    </Card>*/}
-                        {/*                    <br/>*/}
-                        {/*                </>*/}
-                        {/*            )*/}
-                        {/*        }*/}
-                        {/*    </div>*/}
-                        {/*}*/}
-                        {/*{*/}
-                        {/*    !group.groupMembers &&*/}
-                        {/*    <h5>*/}
-                        {/*        No one yet!*/}
-                        {/*    </h5>*/}
-                        {/*}*/}
-                    </div>
+
+                    <h5>Members:</h5>
+                    <br/>
+                    {
+                        group.groupMembers &&
+                        <div className="row">
+                            {
+                                group.groupMembers.map(user =>
+                                    <>
+                                        <Card style={{width: '18rem'}}>
+                                            <Card.Body>
+                                                <Card.Title>{user.username}</Card.Title>
+                                                <Link to={`/profile/${user._id}`}>
+                                                    <Button variant="primary">Go</Button>
+                                                </Link>
+                                            </Card.Body>
+                                        </Card>
+                                        <br/>
+                                    </>
+                                )
+                            }
+                        </div>
+                    }
+                    {
+                        !group.groupMembers &&
+                        <h5>
+                            No one yet!
+                        </h5>
+                    }
+
                 </div>
                 <div className={"col"}></div>
             </div>
@@ -71,17 +72,12 @@ const GroupPage = (
     )
 }
 
-export default GroupPage
-// const stpm = (state) => {
-//     group: state.groupReducer.currentGroup
-// }
-//
-// const dtpm = (dispatch) => {
-//     findGroupById: (groupId) => groupService.findGroupById(groupId)
-//         .then(group => dispatch({
-//             type: "FIND_GROUP_BY_ID",
-//             group: group}))
-// }
+const stpm = (state) => ({
+    group: state.groupReducer.foundGroup
+})
 
+const dtpm = (dispatch) => ({
+    findGroupById: (groupId) => groupActions.findGroupById(dispatch, groupId)
+})
 
-// export default connect(stpm, dtpm)(GroupPage)
+export default connect(stpm, dtpm)(GroupPage)
--- src/services/groups-service.js
+++ src/services/groups-service.js
@@ -1,4 +1,5 @@
 import {PLANTIT_API_URL} from "./node-service";
+// const PLANTIT_API_URL = "http://localhost/4000"
 
 const findGroupById = (groupId) => {
     return fetch(`${PLANTIT_API_URL}/groups/${groupId}`)
